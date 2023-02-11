ec03b9e9ea44eff6cad509b6c5a54f6bbc759614
Rick Rodriguez <rickrod6502@gmail.com>
2023-02-11 13:55:11 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Navbar from './Navbar.svelte';
+
+	import '../app.postcss';
+</script>
+
+	<Navbar />
+	<slot />
---
+++ src/routes/Hamburger.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let open = false;
+</script>
+
+<button on:click={() => (open = !open)} class="flex h-8 w-8 flex-col justify-around z-50 absolute top-2 right-4"
+	>
+		<div
+			class="h-1 w-full rounded bg-white transition-all duration-300 {open
+				? 'translate-y-2 rotate-45'
+				: ''}"
+		/>
+		<div
+			class="h-1 w-full rounded bg-white transition-all duration-300 {open ? 'opacity-0' : ''}"
+		/>
+		<div
+			class="h-1 w-full rounded bg-white transition-all duration-300 {open
+				? '-translate-y-3 -rotate-45'
+				: ''}"
+		/>
+</button>
